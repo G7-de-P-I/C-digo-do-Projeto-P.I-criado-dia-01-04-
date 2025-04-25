bd41c37515c449c0cfe76e8de249ca66271b145f
Rafa_gd <141789950+rafaoiio@users.noreply.github.com>
2025-04-25 09:52:10 -0300

    Puxar Informações

--- tela_desafios.py
+++ tela_desafios.py
@@ -3,12 +3,45 @@ import os
 def limpar_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+<<<<<<< Updated upstream
 pontos1=10
 pontos2=10
 pontos3=10
 pontos4=10
 
 def Tela_desafio():
+=======
+def Tela_desafio(usuario_logado):
+    from datetime import date
+    
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    hoje = datetime.today()
+
+    query = """ SELECT status FROM respostas_desafios WHERE id_usuario = %s AND DATE(data_resposta) = CURDATE()"""
+    cursor.execute(query, (usuario_logado["id"],))
+    resultado = cursor.fetchall()
+    
+    if resultado:
+        print("\n✅ Você já concluiu todos os desafios de hoje!")
+        input("Pressione ENTER para continuar...")
+        Tela_opcoes(usuario_logado)
+    else:
+        print("\nDesafios disponíveis para hoje:")
+        
+        print("1 - DESAFIO ÁGUA")
+        print("2 - DESAFIO RESÍDUOS")
+        print("3 - DESAFIO ENERGIA")
+        print("4 - DESAFIO TRANSPORTE")
+
+        input("\nPressione ENTER para começar os desafios...")
+        Tela_agua(usuario_logado)       
+        
+        
+        
+# Função para a tela do desafio água     
+def Tela_agua(usuario_logado):
+>>>>>>> Stashed changes
     from tela_menu import Tela_menu
     """cria uma tela de DESAFIOS."""
     print("----------------------------------------------------------------------")
@@ -179,18 +212,36 @@ def Tela_agua_residuos():
     if (consumoagua1<150):
         agua = "ALTA SUSTENTABILIDADE"
         pontos1 = 10
+<<<<<<< Updated upstream
         print("Seu nível no DESAFIO 1 - ÁGUA é: ALTA SUSTENTABILIDADE ")
+=======
+        # Salva os dados no banco de dados
+        Salvar_no_Banco(usuario_logado, 1, agua, pontos1, "Concluido", data_agora, consumoagua )
+        print("Seu nível no DESAFIO 1 - ÁGUA é: ALTA SUSTENTABILIDADE")
+>>>>>>> Stashed changes
         
     elif ((consumoagua1>=150) and (consumoagua1<=200)):
         agua = "MODERADA SUSTENTABILIDADE"
         pontos1 = 5
+<<<<<<< Updated upstream
         print("Seu nível no DESAFIO 1 - ÁGUA é: MODERADA SUSTENTABILIDADE ")
+=======
+        # Salva os dados no banco de dados
+        Salvar_no_Banco(usuario_logado, 1, agua, pontos1, "Concluido", data_agora, consumoagua )
+        print("Seu nível no DESAFIO 1 - ÁGUA é: MODERADA SUSTENTABILIDADE")
+>>>>>>> Stashed changes
         
     else:
         agua = "BAIXA SUSTENTABILIDADE"
         pontos1 = 2
+<<<<<<< Updated upstream
         print("Seu nível no DESAFIO 1 - ÁGUA é: BAIXA SUSTENTABILIDADE ")
     
+=======
+        # Salva os dados no banco de dados
+        Salvar_no_Banco(usuario_logado, 1, agua, pontos1, "Concluido", data_agora, consumoagua )
+        print("Seu nível no DESAFIO 1 - ÁGUA é: BAIXA SUSTENTABILIDADE")
+>>>>>>> Stashed changes
     
     print("----------------------------------------------------------------------")
     print("")
@@ -290,8 +341,39 @@ def Tela_agua_residuos():
         Tela_menu()
         
         
+<<<<<<< Updated upstream
     elif (opcao3==1):
         limpar_terminal()
+=======
+        # Determina a sustentabilidade do desafio resíduos
+        if (consumoresiduos1>consumoresiduos2*1.5):
+            residuos = "ALTA SUSTENTABILIDADE"
+            pontos2 = 10
+            # Salva os dados no banco de dados
+            Salvar_no_Banco(usuario_logado, 2, residuos, pontos2, "Concluido", data_agora, consumoresiduos1 )
+            print("Seu nível no DESAFIO 2 - RESÍDUOS é: ALTA SUSTENTABILIDADE")
+    
+        elif ((consumoresiduos1>=consumoresiduos2*1.2) and (consumoresiduos1<=consumoresiduos2*1.5)):
+            residuos = "MODERADA SUSTENTABILIDADE"
+            pontos2 = 5
+            # Salva os dados no banco de dados
+            Salvar_no_Banco(usuario_logado, 2, residuos, pontos2, "Concluido", data_agora, consumoresiduos1 )
+            print("Seu nível no DESAFIO 2 - RESÍDUOS é: MODERADA SUSTENTABILIDADE")
+        elif (consumoresiduos1<consumoresiduos2*0.8):
+            residuos = "BAIXA SUSTENTABILIDADE"
+            pontos2 = 2
+            # Salva os dados no banco de dados
+            Salvar_no_Banco(usuario_logado, 2, residuos, pontos2, "Concluido", data_agora, consumoresiduos1 )
+            print("Seu nível no DESAFIO 2 - RESÍDUOS é: BAIXA SUSTENTABILIDADE")
+        else:
+            residuos = "BAIXA SUSTENTABILIDADE"
+            pontos2 = 2
+            # Salva os dados no banco de dados
+            Salvar_no_Banco(usuario_logado, 2, residuos, pontos2, "Concluido", data_agora, consumoresiduos1 )
+            print("Seu nível no DESAFIO 2 - RESÍDUOS é: BAIXA SUSTENTABILIDADE")
+    
+        # Opção de continuar para o próximo desafio ou voltar para a tela de menu
+>>>>>>> Stashed changes
         print("----------------------------------------------------------------------")
         print("DESAFIO RESÍDUOS PARTE 2")
         print("----------------------------------------------------------------------")
@@ -322,6 +404,7 @@ def Tela_agua_residuos():
         pontos2 = 10
         print("Seu nível no DESAFIO 2 - RESÍDUOS é: ALTA SUSTENTABILIDADE")
     
+<<<<<<< Updated upstream
     elif ((consumoresiduos1>=consumoresiduos2*1.2) and (consumoresiduos1<=consumoresiduos2*1.5)):
         resíduos = "MODERADA SUSTENTABILIDADE"
         pontos2 = 5
@@ -341,6 +424,303 @@ def Tela_agua_residuos():
     print("1 - CONTINUAR")
     print("2 - VOLTAR PARA A TELA MENU")
     
+=======
+        elif (opcao4==1):
+            # Limpa o terminal e chama a tela do desafio energia
+            limpar_terminal()
+            Tela_energia(usuario_logado)
+
+        
+# Função para a tela do desafio energia      
+def Tela_energia(usuario_logado):
+            from tela_menu import Tela_menu
+            data_agora = datetime.now()
+
+            # Tela de explicação do desafio energia
+            print("----------------------------------------------------------------------") 
+            print("DESAFIO 3 - ENERGIA")
+            print("----------------------------------------------------------------------") 
+            print("\nQuase lá! Estamos perto de saber seu nível de sustentabilidade! Saber seu consumo de energia é essencial para completar o desafio.")
+            print("")
+            print("Como verificar seu consumo:")
+            print("\n1. Pegue sua conta de energia -")
+            print("Encontre a fatura referente ao mês atual.")
+            print("\n2. Localize o consumo mensal -")
+            print("Procure a informação de consumo total, geralmente expressa em kWh (quilowatt-hora).")
+         
+            # Pede para o usuário digitar o seu consumo de energia e verifica se um valor válido foi digitado
+            while True:
+                try:
+                    print("")
+                    kwh = int(input("Digite quanto você gasta de energia em Kwh: "))
+                    break
+                except ValueError:
+                    print("")
+                    print("Erro: Digite um valor numérico válido para o consumo de energia.")
+     
+            print("")
+            print("----------------------------------------------------------------------")
+         
+            # Determina a sustentabilidade do desafio energia
+            if kwh < 5:
+                energia = "ALTA SUSTENTABILIDADE"
+                pontos3 = 10
+                # Salva os dados no banco de dados
+                Salvar_no_Banco(usuario_logado, 3, energia, pontos3, "Concluido", data_agora, kwh )
+                print("Seu nível no DESAFIO 3 - ENERGIA É: ALTA SUSTENTABILIDADE")
+             
+            elif 5 <= kwh <= 10:
+                energia = "MODERADA SUSTENTABILIDADE"
+                pontos3 = 5
+                # Salva os dados no banco de dados
+                Salvar_no_Banco(usuario_logado, 3, energia, pontos3, "Concluido", data_agora, kwh )
+                print("Seu nível no DESAFIO 3 - ENERGIA É: MODERADA SUSTENTABILIDADE")
+             
+            else:
+                energia = "BAIXA SUSTENTABILIDADE"
+                pontos3 = 2
+                # Salva os dados no banco de dados
+                Salvar_no_Banco(usuario_logado, 3, energia, pontos3, "Concluido", data_agora, kwh )
+                print("Seu nível no DESAFIO 3 - ENERGIA É: BAIXA SUSTENTABILIDADE")
+     
+            # Opção de continuar para o próximo desafio ou voltar para a tela de menu
+            print("----------------------------------------------------------------------") 
+            print("")
+            print("Escolha uma opção:")
+            print("")
+            print("1 - CONTINUAR")
+            print("2 - VOLTAR PARA A TELA MENU")
+         
+            # Pede para o usuário digitar a opção escolhida e verifica se um valor válido foi digitado
+            while True:
+                try:
+                    print("")
+                    opcao8 = int(input("Digite a opção escolhida: "))
+                    break
+                except ValueError:
+                    print("")
+                    print("Erro: Digite uma opção válida.")
+     
+            print("")
+
+            if opcao8 == 2:
+                # Limpa o terminal e chama a tela de menu
+                limpar_terminal()
+                Tela_menu(usuario_logado)
+         
+            elif opcao8 == 1:
+                # Limpa o terminal e chama a tela do desafio transporte
+                limpar_terminal()
+                Tela_transporte(usuario_logado)
+                
+                
+# Função para a tela desafio transporte            
+def Tela_transporte(usuario_logado): 
+    data_agora = datetime.now()
+
+    # Tela de explicação do desafio transporte
+    print("----------------------------------------------------------------------")
+    print("DESAFIO 4 - TRANSPORTE")
+    print("----------------------------------------------------------------------")
+    print("")
+    print("Excelente! Você passou por quase todos os desafios, conclua o desafio 4 para descobrir o quão sustentável você é.")
+    print("")
+    print("Qual meio de transporte você mais utilizou hoje?")
+    print("")
+    print("Escolha uma opção:")
+    print("")
+    print("1 - Sem gasto de combustíveis fósseis (a pé, bicicleta, patinete ou outro meio)")
+    print("2 - Uso misto de transporte público e privado (ônibus, carona ou outro meio)")
+    print("3 - Uso exclusivo e privado (veículo próprio)")
+    print("")
+    
+    # Pede para o usuário digitar o seu transporte mais utilizado no dia atual e verifica se um valor válido foi digitado
+    while True:
+        try:
+            meiotransporte = int(input("Digite uma das opções: "))
+        
+            if meiotransporte == 1:
+                transporte = "Sem gasto de combustíveis fósseis"
+                pontos4 = 10
+                print("Seu nível no DESAFIO 4 - TRANSPORTE É: ALTA SUSTENTABILIDADE")
+
+            elif meiotransporte == 2:
+                transporte = "Uso misto de transporte público e privado"
+                pontos4 = 5
+                print("Seu nível no DESAFIO 4 - TRANSPORTE É: MODERADA SUSTENTABILIDADE")
+
+            elif meiotransporte == 3:
+                transporte = "Uso exclusivo e privado"
+                pontos4 = 2
+                print("Seu nível no DESAFIO 4 - TRANSPORTE É: BAIXA SUSTENTABILIDADE\n")
+
+            else:
+                print("Digite uma opção válida (1, 2 ou 3).\n")
+                continue  # volta pro início do loop
+
+            # Se chegou aqui, é porque a opção foi válida
+            # Salva no banco de dados
+            Salvar_no_Banco(usuario_logado, 4, transporte, pontos4, "Concluido", data_agora, 0)
+            break
+
+        except ValueError:
+            print("\nErro: Digite um número válido.\n")
+                
+    # Limpa o terminal e chama a tela de opções
+    limpar_terminal()
+    print("\nMUITO BEM, DESAFIOS TERMINADOS\n")
+    Tela_opcoes(usuario_logado)
+        
+    
+            
+
+def Tela_opcoes(usuario_logado):
+    from tela_historico import Tela_historico
+    print("")
+    print("----------------------------------------------------------------------")
+            # Nível de sustentabilidade para transporte
+                
+     
+    print("--------------------------------------------------------------------")
+    print("\nDESAFIOS CONCLUÍDOS\n")
+    print("Escolha uma opção:")
+    print("1 - LEVANTAMENTO DE DADOS")
+    print("2 - SUSTENTABILIDADE MENSAIS")
+    print("3 - SUSTENTABILIDADE DIÁRIA")
+    print("4 - VER TABELA DE SUSTENTABILIDADE")
+    print("5 - VOLTAR PARA A TELA DE MENU")
+            
+    conexao = conectar()
+    cursor = conexao.cursor()
+    
+    comando = "SELECT pontuacao FROM respostas_desafios WHERE id_usuario = %s"
+    
+    cursor.execute(comando, (usuario_logado["id"],))
+    resultado = cursor.fetchall()
+    
+    cursor.close()
+    conexao.close()
+    desafios_diarios = [linha[0] if isinstance(linha, tuple) else linha['pontuacao'] for linha in resultado]
+
+    
+    while True:
+        try:
+            print("")
+            opcao10 = int(input("Digite a opção escolhida: "))
+            
+        except ValueError:
+            print("")
+            print("Erro: Digite uma opção válida.")
+            
+        if(opcao10 == 1):
+            limpar_terminal()
+            Tela_dados(usuario_logado, desafios_diarios[0], desafios_diarios[1], desafios_diarios[2], desafios_diarios[3])
+            break
+        elif(opcao10 == 2):
+            limpar_terminal()
+            Tela_mensal(usuario_logado)
+            break
+        elif(opcao10 == 3):
+            limpar_terminal()
+            Tela_dia(usuario_logado, desafios_diarios[0], desafios_diarios[1], desafios_diarios[2], desafios_diarios[3])
+            break
+        elif(opcao10 == 4):
+            limpar_terminal()
+            Tela_historico(usuario_logado)
+            break
+            
+
+def Salvar_no_Banco(usuario_logado, id_desafio, resposta, pontuacao, status, data_resposta, valor):
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor()
+    
+    
+        comando = "INSERT INTO respostas_desafios(id_usuario, id_desafio, respostas, pontuacao, status, data_resposta, valor) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+        valores = (usuario_logado['id'], id_desafio, resposta, pontuacao, status, data_resposta, valor )
+    
+        cursor.execute(comando, valores)
+        conexao.commit()
+        print("✅ Resposta salva com sucesso!")
+        
+    except Exception as e:
+        print(f"⚠️  Erro ao salvar resposta: {e}")
+        
+    finally:
+        if cursor:
+            cursor.close()
+        if conexao:
+            conexao.close()
+            
+
+# Função para a tela de sustentabilidade mensal      
+def Tela_mensal(usuario_logado):
+    from datetime import date
+
+
+    print("----------------------------------------------------------------------")
+    print("SUSTENTABILIDADE MENSAL")
+    print("----------------------------------------------------------------------\n")
+
+# Conectar ao banco
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    id_usuario = usuario_logado["id"]
+
+# Selecionar as pontuações diárias
+    comando_pontuacoes = "SELECT pontuacao FROM respostas_desafios WHERE id_usuario = %s"
+    cursor.execute(comando_pontuacoes, (id_usuario,))
+    resultado = cursor.fetchall()
+
+# Lista com os pontos
+    desafios_diarios = [linha['pontuacao'] for linha in resultado]
+
+    if desafios_diarios:
+        Smensal = sum(desafios_diarios) / len(desafios_diarios)
+        nivel = ""
+
+        if 30 <= Smensal <= 40:
+            nivel = "ALTA"
+        elif 20 <= Smensal < 30:
+            nivel = "MODERADA"
+        else:
+            nivel = "BAIXA"
+
+        print(f"Sua média de pontos mensal foi: {Smensal:.2f}")
+        print(f"NÍVEL DE SUSTENTABILIDADE MENSAL: {nivel}!\n")
+
+    # Atualiza ou insere
+        data_atual = date.today()
+        verificar_existencia = """
+            SELECT id FROM resultados_desafios
+            WHERE id_usuario = %s AND MONTH(data) = MONTH(%s) AND YEAR(data) = YEAR(%s)
+        """
+        cursor.execute(verificar_existencia, (id_usuario, data_atual, data_atual))
+        resultado_existente = cursor.fetchone()
+
+        if resultado_existente:
+            atualizar = """
+                UPDATE resultados_desafios
+                SET resultado_mensal = %s, data = %s
+                WHERE id = %s
+            """
+            cursor.execute(atualizar, (nivel, data_atual, resultado_existente['id']))
+        else:
+            inserir = """
+                INSERT INTO resultados_desafios (id_usuario, resultado_mensal, data)
+                VALUES (%s, %s, %s)
+            """
+            cursor.execute(inserir, (id_usuario, nivel, data_atual))
+
+    conexao.commit()
+
+# Sempre feche a conexão
+    cursor.close()
+    conexao.close()
+    
+
+    # Menu de opções
+>>>>>>> Stashed changes
     while True:
         try:
             print("")
@@ -540,6 +920,23 @@ def Tela_de_levantamento_de_dados_diario():
         from tela_menu import Tela_menu
         from tela_principal import Tela_de_saida
         
+<<<<<<< Updated upstream
+=======
+        conexao = conectar()
+        cursor = conexao.cursor()
+    
+        comando = "SELECT pontuacao FROM respostas_desafios WHERE id_usuario = %s"
+    
+        cursor.execute(comando, (usuario_logado["id"],))
+        resultado = cursor.fetchall()
+    
+        cursor.close()
+        conexao.close()
+
+        
+        #desafios_diarios = [sum(linha) for linha in resultado]
+        
+>>>>>>> Stashed changes
         total_pontos = pontos1 + pontos2 + pontos3 + pontos4 
         if total_pontos > 0:
             porcentagem1 = (pontos1 / total_pontos) * 100
--- tela_historico.py
+++ tela_historico.py
@@ -10,6 +10,7 @@ def Tela_historico():
     from tela_menu import Tela_menu
     from tela_desafios import Tela_desafio
     
+<<<<<<< Updated upstream
     console = Console()
     
     t = Table(title = "\nHistórico de Sustentabilidade")
@@ -23,9 +24,58 @@ def Tela_historico():
     
     console.print(t)
     
+=======
+    
+    print("--------------------------------")
+    print("1 - DESAFIO ÁGUA")
+    print("2 - DESAFIO ÁGUA")
+    print("3 - DESAFIO ÁGUA")
+    print("4 - DESAFIO ÁGUA")
+    print("--------------------------------")
+    print(" ")
+    data_desejada_string = input("Digite a data que deseja bucar suas informções de sustentabilidade!! (DD/MM/AAAA): ")
+    
+    data_desejada = datetime.strptime(data_desejada_string, "%d/%m/%Y").date()
+    
+    conexao = conectar()
+    cursor = conexao.cursor(dictionary=True)
+    comando = "SELECT id_desafio, respostas, pontuacao, data_resposta, valor FROM respostas_desafios WHERE id_usuario = %s AND DATE(data_resposta) = %s"
+    cursor.execute(comando, (usuario_logado["id"], data_desejada))
+    dados = cursor.fetchall()
+    
+    console = Console()
+    
+    try:
+        if dados:
+            t = Table(title="\nHistórico de Sustentabilidade")
+
+            t.add_column("N° DESAFIO", justify="left", style="white")
+            t.add_column("RESPOSTA", justify="center", style="white")
+            t.add_column("PONTUAÇÃO", justify="left", style="white")
+            t.add_column("VALOR FORNECIDO", justify="center", style="white")
+            t.add_column("DATA", justify="center", style="white")
+    
+        
+            for linha in dados:
+                desafio = str(linha['id_desafio'])
+                resposta = str(linha['respostas'])
+                valor = str(linha['valor'])
+                pontuacao = str(linha['pontuacao'])
+                data_formatada = linha['data_resposta'].strftime('%d/%m/%Y %H:%M')
+                
+                t.add_row(desafio, resposta, pontuacao,valor, data_formatada)
+                
+            console.print(t) 
+        else:
+            print(f"\nNenhum desafio foi respondido em {data_desejada.strftime('%d/%m/%Y')}.")
+    except ValueError:
+        print("Formato de data inválido. Use o formato DD/MM/AAAA.")
+        
+       
+>>>>>>> Stashed changes
     
     while True:
-        data_input = input("\nDigite uma data (DD/MM/AAAA), caso deseje alterar as informações dos desafios!(PARA RETORNAR PRESS ENTER):  ")
+        data_input = input("\n Caso deseje alterar algum desafio   ")
         
         if(data_input == ""):
             limpar_terminal()
